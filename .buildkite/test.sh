9f7a0fd269c865d205c92dcea9fa5a104d61bbf4
Graham Moore <graham.moore@reevoo.com>
2018-12-07 14:04:46 +0000

    Add back tests

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -6,13 +6,3 @@ bundle exec reevoocop
 
 bundle exec ruby-audit check
 bundle exec bundle-audit check --update
-
-# wait for database to be created
-sleep 5
-# bootstrap data store
-bundle exec slodd -g reevoo/revieworld -t c433ef4ebba939bdf20f72627dd137904c44f28a
-bundle exec rake db:migrate
-bundle exec rake db:test:prepare
-
-# running actual tests
-bundle exec rake
